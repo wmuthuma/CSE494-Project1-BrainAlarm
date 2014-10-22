5f6658e29ae4a1a4d52590893c94d21eb1d8534f
Nathaniel Mendoza <namendoz@asu.edu>
2014-10-22 14:22:24 -0700

    + preliminary Alarm object

---
+++ BrainAlarm/BrainAlarm/BAAlarmModel.h
@@ -0,0 +1,38 @@
+//
+//  BAAlarmModel.h
+//  BrainAlarm
+//
+//  Created by Nathaniel Mendoza on 10/22/14.
+//  Copyright (c) 2014 ___CSE494___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BAAlarmModel : NSObject
+
+typedef enum
+{
+    Math,
+    JJ
+} TaskType;
+
+typedef enum
+{
+    Sun,
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat
+   
+} Days;
+
+@property NSDate *alarmTime;
+@property TaskType type;
+@property NSArray *daysActive;
+
+
+
+
+@end
